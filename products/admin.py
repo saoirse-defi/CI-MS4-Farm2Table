9783f870e4978200d56bff2842d3a5508fc4fc11
saoirse-defi <saoirsef@yahoo.co.uk>
2021-08-13 12:01:09 +0000

    productAdmin & categoryAdmin classes created

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 from .models import Product, Category
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendy_name',
+        'name'
+    )
+
+
 admin.site.register(Product)
 admin.site.register(Category)
