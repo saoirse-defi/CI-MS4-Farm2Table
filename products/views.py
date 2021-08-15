30e16f24d904ef70e243efe0c4a62d7f6f449cf8
saoirse-defi <saoirsef@yahoo.co.uk>
2021-08-15 10:11:36 +0000

    add sort functionality

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 
 def all_products(request):
@@ -9,8 +9,29 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    category = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -23,6 +44,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': category,
     }
 
     return render(request, 'products/products.html', context)
@@ -34,6 +56,6 @@ def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'products': product,
+        'product': product,
     }
     return render(request, 'products/product_detail.html', context)
