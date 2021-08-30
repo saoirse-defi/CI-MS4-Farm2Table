210d841221e76dda2d6e7ac07ca39d48555d8384
saoirse-defi <saoirsef@yahoo.co.uk>
2021-08-30 14:49:00 +0000

    add stripe logic for card element

---
+++ checkout/static/checkout/js/checkout.js
@@ -0,0 +1,28 @@
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
+var elements = stripe.elements();
+var card = elements.create('card');
+
+var style = {
+    base: {
+        iconColor: '#000',
+        color: '#fff',
+        fontWeight: '500',
+        fontFamily: 'Roboto, Open Sans, Segoe UI, sans-serif',
+        fontSize: '16px',
+        fontSmoothing: 'antialiased',
+        ':-webkit-autofill': {
+          color: '#fce883',
+        },
+        '::placeholder': {
+          color: '#87BBFD',
+        },
+      },
+      invalid: {
+        iconColor: '#FF4444',
+        color: '#FF4444',
+      }
+}
+
+card.mount('#card-element', {style: style});
