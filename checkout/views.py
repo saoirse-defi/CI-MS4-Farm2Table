317650aecf79e5dde87033c3848a2579e8798fae
saoirse-defi <saoirsef@yahoo.co.uk>
2021-08-30 14:50:20 +0000

    add context stripe public key/secret

--- checkout/views.py
+++ checkout/views.py
@@ -14,7 +14,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JUBLoJiiZl6riemjloDASbxq8eXcDOPrvAe0Tbi9ng0uW4S9LIjwX6r44r4a0Qv0W7u3zea0dlD5akWTk7FvBDd00grgIGrUr',
+        'client_secret': 'sk_test_51JUBLoJiiZl6riemDFRe7fWLn30zGHnPhTr71v9nk1o8pXyiGPo5AiyC9JHaUhurlyNeorizwkGZ8RxPVsDw5hwF00IYWXXp4U'
     }
 
     return render(request, template, context)
