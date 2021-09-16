42fa74aea36569d42ffad7f88fa3cfc07ad8ead4
saoirse-defi <saoirsef@yahoo.co.uk>
2021-09-16 13:07:44 +0000

    add original_bag, stripe_pid to admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -12,7 +12,8 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total')
+                       'grand_total', 'original_bag', 
+                       'stripe_pid')
 
     fields = ('order_number', 'date',
               'full_name', 'email',
@@ -20,11 +21,13 @@ class OrderAdmin(admin.ModelAdmin):
               'postcode', 'town', 'street_address1',
               'street_address2', 'county',
               'delivery_cost', 'order_total',
-              'grand_total')
+              'grand_total', 'original_bag', 
+              'stripe_pid')
 
     list_display = ('order_number', 'date',
                     'full_name', 'order_total',
-                    'delivery_cost', 'grand_total')
+                    'delivery_cost', 'grand_total', 
+                    'original_bag', 'stripe_pid')
 
     ordering = ('-date', )
 
