53fbeb56cee246ee3bbb6988220994c6e6e27658
saoirse-defi <saoirsef@yahoo.co.uk>
2021-09-21 11:13:03 +0000

    add userProfileForm

---
+++ profile/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user', )
+
+    def __init__(self, *args, **kwargs):
+        """Adds placeholders and classes,
+            remove auto-gen labels and sets
+            autofocus on first field."""
+
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'Phone',
+            'default_street_address1': 'Street Address Line 1',
+            'default_street_address2': 'Street Address Line 2',
+            'default_town': 'Town/City',
+            'default_county': 'County',
+            'default_postcode': 'Eircode',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
+            self.fields[field].widget.attrs['placeholder'] = False
--- profile/views.py
+++ profile/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+
+from .models import UserProfile
+from .forms import UserProfileForm
 
 
+@login_required
 def profile(request):
     """Displays user profile."""
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
     template = "profile/profile.html"
-    context = {}
+    context = {
+        'form': form,
+        'orders': orders,
+    }
 
     return render(request, template, context)
