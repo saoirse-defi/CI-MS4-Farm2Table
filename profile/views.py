61773e89c5449bf1f14204702c21d8660dac8e26
saoirse-defi <saoirsef@yahoo.co.uk>
2021-09-23 10:21:46 +0000

    add order_history to profile

--- profile/views.py
+++ profile/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
@@ -10,12 +11,19 @@ def profile(request):
     """Displays user profile."""
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile updated successfully.")
+
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
     template = "profile/profile.html"
     context = {
         'form': form,
         'orders': orders,
+        'on_profile_page': True,
     }
 
     return render(request, template, context)
