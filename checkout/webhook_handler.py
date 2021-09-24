58c7ca44a70210fd5112a027cca1e7199875e9f1
saoirse-defi <saoirsef@yahoo.co.uk>
2021-09-24 10:55:03 +0000

    add send email class method, add default from email

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -16,6 +16,24 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+    
+    def _send_confirmation_email(self, order):
+        """ Sends the user a confirmation email. """
+        customer_email = order.email
+        subject = render_to_string(
+            'checkout/templates/checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/templates/checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order,
+             'contact_email': settings.DEFAULT_FROM_EMAIL})
+        
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [customer_email]
+        )
 
     def handle_event(self, event):
         """Handle a generic/unknown/unexpected webhook event"""
@@ -78,6 +96,7 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
+            self._send_confirmation_email(order)
             return HttpResponse(
                 content=(f'Webhook received: {event["type"]} | SUCCESS: '
                          'Verified order already in database'),
@@ -123,6 +142,7 @@ class StripeWH_Handler:
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
+        self._send_confirmation_email(order)
         return HttpResponse(
             content=(f'Webhook received: {event["type"]} | SUCCESS: '
                      'Created order in webhook'),
