f5ad5a616df94594fe018d368900c12fb5fed83c
saoirse-defi <saoirsef@yahoo.co.uk>
2021-09-27 17:22:59 +0000

    add edit_product view, urls, template

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.decorators import login_required
 
 from .models import Product, Category
 from .forms import ProductForm
@@ -79,5 +80,28 @@ def add_product(request):
     template = 'products/add_product.html'
     context = {
         'form': form
+    }
+    return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edits an existing product the profile has created. """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Product successfully updated!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, f'Failed to update {product.name}, please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
     }
     return render(request, template, context)
