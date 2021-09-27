d1146bcc1aaa907684bd4baba862dbae0362254b
saoirse-defi <saoirsef@yahoo.co.uk>
2021-09-27 13:57:00 +0000

    add error messages for invalid form submission

--- profile/views.py
+++ profile/views.py
@@ -17,8 +17,11 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, "Profile updated successfully.")
+        else:
+            messages.error(request, 'Profile Update Failed: Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
 
-    form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
     template = "profile/profile.html"
     context = {
