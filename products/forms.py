382616bbcb9a51335482e1834d9c3c3445310f42
saoirse-defi <saoirsef@yahoo.co.uk>
2021-09-29 09:45:27 +0000

    tried to add placeholder field, fix multiple input issue, still not fixed

--- products/forms.py
+++ products/forms.py
@@ -6,15 +6,27 @@ from .models import Product, Category
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        exclude = ('has_sizes', 'rating', 'sku')
+        exclude = ('category', 'has_sizes', 'rating', 'sku')
 
     image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileUnit)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        categories = Category.objects.all()
-        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+        placeholders = {
+            'price': 'Price per 500g'
+        }
 
-        self.fields['category'].choices = friendly_names
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded'
+        self.fields['name'].widget.attrs['autofocus'] = True
+
+        for field in self.fields:
+            if field == 'price':
+                placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+                self.fields[field].widget.attrs['placeholder'] = False
+        #categories = Category.objects.all()
+        #friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        #self.fields['category'].choices = friendly_names
+        #for field_name, field in self.fields.items():
+            # field.widget.attrs['class'] = 'border-black rounded'
