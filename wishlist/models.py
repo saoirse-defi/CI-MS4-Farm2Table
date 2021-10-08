d39c212181c753cc02adcaa61c7c5ebd566d1645
saoirse-defi <saoirsef@yahoo.co.uk>
2021-10-08 16:51:23 +0000

    create wishlist app, wishlist model

---
+++ wishlist/models.py
@@ -0,0 +1,14 @@
+import uuid
+from django.db import models
+from profile.models import UserProfile
+
+# Create your models here.
+
+
+class Wishlist(models.Model):
+    wishlist_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey('UserProfile', null=False, blank=False, default=None, on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.wishlist_id
