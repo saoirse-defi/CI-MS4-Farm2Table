26eb268c5146934d4b6edb06221292b310fe082b
saoirse-defi <saoirsef@yahoo.co.uk>
2021-10-10 11:27:24 +0000

    create wishlist template, view, url

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def wishlist(request):
+
+    wishlist_all = Wishlist.objects.all()
+    my_wishlist = []
+
+    for wishlist_item in wishlist_all:
+        if wishlist_item.user == request.user:
+            my_wishlist.append(wishlist_item)
+
+    template = "wishlist/wishlist.html"
+    context = {
+        'my_wishlist': my_wishlist,
+    }
+    return render(request, template, context)
