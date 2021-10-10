18cfb2a79a628eea57530ae480b6dec2e8eddedf
saoirse-defi <saoirsef@yahoo.co.uk>
2021-10-10 11:27:49 +0000

    limit store ownership to one per user

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,9 @@
+import uuid
 from django.contrib import admin
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('create_store/', views.create_store, name='create_store'),
-    path('view_store/<int:store_id>/', views.view_store, name='view_store'),
+    path('<uuid:store_id>/', views.view_store, name='view_store'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 from .forms import StoreRegisterForm
 from profile.models import UserProfile
@@ -12,13 +13,19 @@ from .models import Store
 @login_required
 def create_store(request):
     if request.method == 'POST':
+        stores = Store.objects.all()
         form = StoreRegisterForm(request.POST)
+        for _store in stores:
+            if _store.user == request.user:
+                messages.error(request, 'Organisation creation failed, you can only have 1 store linked to each account.')
+                return redirect(reverse('view_store', args=[_store.store_id, ]))
+
         if form.is_valid():
             store = form.save(commit=False)
             store.user = request.user
             store.save()
             messages.success(request, 'Store Organisation Created!')
-            return redirect('/')
+            return redirect(reverse('view_store', args=[_store.store_id, ]))
         else:
             messages.error(request, 'Organisation creation failed, please check form details.')
     else:
