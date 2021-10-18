aa000a286a7772ddd9c6fbb9fd15c585f2cd970a
saoirse-defi <saoirsef@yahoo.co.uk>
2021-10-18 08:22:54 +0000

    add remove wishlist_item functionality

--- wishlist/views.py
+++ wishlist/views.py
@@ -24,7 +24,7 @@ def wishlist(request):
         'my_wishlist': my_wishlist,
         'wishlist_all': wishlist_all,
     }
-    
+
     return render(request, template, context)
 
 
@@ -36,5 +36,17 @@ def add_to_wishlist(request, sku):
     Wishlist.objects.update_or_create(
         user=profile, product=product
     )
+
+    messages.success(request, 'Item successfully added to wishlist.')
+
+    return redirect(reverse('wishlist'))
+
+
+def delete_from_wishlist(request, wishlist_id):
+    wishlist_item = get_object_or_404(Wishlist, pk=wishlist_id)
+
+    wishlist_item.delete()
+
+    messages.success(request, 'Item successfully deleted from wishlist.')
 
     return redirect(reverse('wishlist'))
