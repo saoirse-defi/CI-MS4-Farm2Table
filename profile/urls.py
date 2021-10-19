2cb1b694daf277caf9c8a5ca511154641ff17b15
saoirse-defi <saoirsef@yahoo.co.uk>
2021-10-19 18:54:41 +0000

    fix pylint error

--- profile/urls.py
+++ profile/urls.py
@@ -1,11 +1,11 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.view_profile, name='view_profile'),
     path('edit_profile', views.edit_profile, name='edit_profile'),
-    path('order_history/<order_number>', views.order_history, name='order_history'),
+    path('order_history/<order_number>', views.order_history,
+         name='order_history'),
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
