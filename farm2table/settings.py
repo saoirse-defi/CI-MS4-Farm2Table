495b7ae5999c8fe6f81d1c8cf866b37e292b2ab7
saoirse-defi <saoirsef@yahoo.co.uk>
2021-10-23 13:35:37 +0000

    change debug back to true

--- farm2table/settings.py
+++ farm2table/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p+l3@33i*8h(f@vol!pc^p8xgkhvrh))2uel3neqf-6poqoz--'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
