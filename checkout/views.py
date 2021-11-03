2fbe67860f5828442640d81bcb9d7f8c5b855a13
saoirse-defi <saoirsef@yahoo.co.uk>
2021-11-03 14:05:26 +0000

    attempt to correctly filter

--- checkout/views.py
+++ checkout/views.py
@@ -144,7 +144,7 @@ def checkout_success(request, order_number):
     ''' Handles successful checkout. '''
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-    order_items = OrderLineItem.objects.filter(order_number__exact=order)
+    order_items = OrderLineItem.objects.filter(order__order_number=order_number)
 
     if request.user.is_authenticated:
         profile = UserProfile.objects.get(user=request.user)
