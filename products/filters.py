763014a640fc6ec054908ea41b1931ba3082f011
saoirse-defi <saoirsef@yahoo.co.uk>
2021-11-03 09:29:59 +0000

    pylint error fix

--- products/filters.py
+++ products/filters.py
@@ -7,4 +7,8 @@ class ProductFilter(django_filters.FilterSet):
     class Meta:
         model = Product
         fields = '__all__'
-        exclude = ['sku', 'seller_store', 'name', 'description', 'price', 'has_sizes', 'organic', 'rating', 'image_url', 'image']
+        exclude = ['sku', 'seller_store',
+                   'name', 'description',
+                   'price', 'has_sizes',
+                   'organic', 'rating',
+                   'image_url', 'image']
