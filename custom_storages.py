164b78be4b4b1ee56bde08296a5c9d2ba7d4c601
saoirse-defi <saoirsef@yahoo.co.uk>
2021-11-05 14:33:51 +0000

    Add Doc string

--- custom_storages.py
+++ custom_storages.py
@@ -3,10 +3,12 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
+    """ Storage class for static files, AWS s3 bucket. """
     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
+    """ Storage class for media files, AWS s3 bucket. """
     location = settings.MEDIAFILES_LOCATION
 
 
