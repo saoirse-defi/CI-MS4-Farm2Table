d41eb95aec57131b9ea995d0dd2a259f4b0c5fc2
saoirse-defi <saoirsef@yahoo.co.uk>
2021-11-24 12:13:24 +0000

    fixing exception issue

--- farm2table/views.py
+++ farm2table/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 
 
-def error_handler_404(request):
+def error_handler_404(request, exception):
+    print(exception)
     return render(request, '404.html', status=404)
 
 
-def error_handler_403(request):
+def error_handler_403(request, exception):
+    print(exception)
     return render(request, '403.html', status=403)
 
 
-def error_handler_400(request):
+def error_handler_400(request, exception):
+    print(exception)
     return render(request, '400.html', status=400)
 
 
