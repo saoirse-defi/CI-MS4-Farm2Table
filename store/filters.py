e5074bc551213fd73ede6b0f8c61d3cbac399794
saoirse-defi <saoirsef@yahoo.co.uk>
2021-11-24 14:13:23 +0000

    add description to filter exclude

--- store/filters.py
+++ store/filters.py
@@ -7,5 +7,8 @@ class StoreFilter(django_filters.FilterSet):
     class Meta:
         model = Store
         fields = '__all__'
-        exclude = ['store_id', 'user', 'name', 'email', 'phone_number', 'street_address1', 'street_address2', 'town', 'country', 'postcode', 'organic', 'iban', 'rating', 'image_url', 'image']
+        exclude = ['store_id', 'user', 'name', 'description', 'email',
+                   'phone_number', 'street_address1', 'street_address2',
+                   'town', 'country', 'postcode', 'organic', 'iban',
+                   'rating', 'image_url', 'image']
         order_by = 'name'
