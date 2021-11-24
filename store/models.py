b028a1b55d5a36c30d1d818209e515b3e7ee47f6
saoirse-defi <saoirsef@yahoo.co.uk>
2021-11-24 13:41:35 +0000

    add store description

--- store/models.py
+++ store/models.py
@@ -14,6 +14,7 @@ class Store(models.Model):
     user = models.ForeignKey(UserProfile, null=True, blank=False,
                              default=None, on_delete=models.CASCADE)
     name = models.CharField(max_length=254)
+    description = models.TextField(max_length=1024, blank=False, default="")
     email = models.CharField(max_length=254)
     phone_number = models.CharField(max_length=20,
                                     null=True, blank=True)
