e26a39116bd384f9ca8de0b634c9648b66ff8c45
saoirse-defi <saoirsef@yahoo.co.uk>
2021-11-25 11:54:36 +0000

    match product widget to store widget

--- products/widgets.py
+++ products/widgets.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class ProductClearableFileUnit(ClearableFileInput):
-    clear_checkbox_label = _('Remove Image')
+    clear_checkbox_label = _('')
     initial_text = _('Current Image')
-    input_text = _('Change')
+    input_text = _('')
     template_name = 'products/custom_widget_templates/product_clearable_file_unit.html'
